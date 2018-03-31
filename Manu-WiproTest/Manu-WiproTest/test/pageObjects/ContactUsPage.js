84bf1ea536f4aa9372b4391884c2bf36feff07cf
send2mann <37663870+send2mann@users.noreply.github.com>
2018-03-31 21:11:15 +0100

    Update ContactUsPage.js

--- Manu-WiproTest/Manu-WiproTest/test/pageObjects/ContactUsPage.js
+++ Manu-WiproTest/Manu-WiproTest/test/pageObjects/ContactUsPage.js
@@ -1,11 +1,11 @@
-var { config } = require('../../wdio.conf');
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var assert = require('chai').assert;
-var expect = require('chai').expect;
+let { config } = require('../../wdio.conf');
+let chai = require('chai');
+let chaiAsPromised = require('chai-as-promised');
+let assert = require('chai').assert;
+let expect = require('chai').expect;
 
-var Utils = require('../utils/Utils');
-var utils = new Utils();
+let Utils = require('../utils/Utils');
+const expectedTitleText = "Lloyds Bank - Contact Information - Phone Numbers, Branch Info & More";
 
 const selection1 = "//div[@class='title-wrapper']/h3[contains(text(),'I want to call Lloyds Bank')]";
 const selection2 = "//div[@class='title-wrapper']/h3[contains(text(),'My card is lost or stolen')]";
@@ -14,25 +14,19 @@ const selection4 = "//div[@class='title-wrapper']/h3[contains(text(),'Ask a ques
 const selection5 = "//div[@class='title-wrapper']/h3[contains(text(),'Self Service')]";
 const selection6 = "//div[@class='title-wrapper']/h3[contains(text(),'Make a complaint')]";
 
-const expectedTitleText = "Lloyds Bank - Contact Information - Phone Numbers, Branch Info & More";
-
 class ContactUsPage {
 
-assertContactUsPageTitle(){	
-		 
+assertContactUsPageTitle(){
 		 utils.assertTitle(expectedTitleText);
-
 		}
 
-assertPageElements(){
-		
+assertSelections(){
 		assert.isTrue(browser.isVisible(selection1));
 		assert.isTrue(browser.isVisible(selection2));
 		assert.isTrue(browser.isVisible(selection3));
 		assert.isTrue(browser.isVisible(selection4));
 		assert.isTrue(browser.isVisible(selection5));
 		assert.isTrue(browser.isVisible(selection6));
-
 }
 }
 module.exports =  ContactUsPage;
